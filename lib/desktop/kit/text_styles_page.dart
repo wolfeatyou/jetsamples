62f8e11e4403b9c471e3312bf4fe2ec38df50fd5
wolfeatyou <fivefong@gmail.com>
2020-07-08 22:52:15 +0300

    tabs

--- lib/desktop/kit/text_styles_page.dart
+++ lib/desktop/kit/text_styles_page.dart
@@ -4,7 +4,6 @@ import 'package:jetkit/jetkit.dart';
 import 'package:flutter_highlight/flutter_highlight.dart';
 import 'package:flutter_highlight/themes/darcula.dart';
 
-
 class TextStylesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -59,7 +58,7 @@ class TextStylesPage extends StatelessWidget {
 }
 
      ''';
-
+    JetThemeData theme = MaterialJetTheme.getThemeData(Theme.of(context));
     return NotificationListener<ScrollNotification>(
       onNotification: (scrollState) {
         print(scrollState);
@@ -67,49 +66,57 @@ class TextStylesPage extends StatelessWidget {
       },
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
-
         child: JetPanel(
             color: Color(0xffF7F9FA),
             title: "Typography",
             child: ConstrainedBox(
               constraints: BoxConstraints(maxWidth: 900),
-              child: Column(
-                  children: [
-                    JetTab(
-                      items: [
-                        JetTabItem(text: 'All'),
-                        JetTabItem(text: 'All'),
-                        JetTabItem(text: 'All'),
-                        JetTabItem(text: 'All')
-                      ],
-                    ),
-                    JetTab(
-                      styles: MaterialJetTheme
-                          .getThemeData(Theme.of(context)).tabThemeClassic.styles,
-                      items: [
-                        JetTabItem(text: 'All'),
-                        JetTabItem(text: 'All'),
-                        JetTabItem(text: 'All'),
-                        JetTabItem(text: 'All')
-                      ],
-                    ),
+              child: Column(children: [
+                JetTab(
+                  uppercase: true,
+                  items: [
+                    JetTabItem(text: 'All', selected: true),
+                    JetTabItem(text: 'Video'),
+                    JetTabItem(text: 'Stores'),
+                    JetTabItem(text: 'Settings'),
+                    JetTabItem(text: 'Tools'),
+                    JetTabItem(text: 'Images')
+                  ],
+                ),
+                Container(
+                  height: 30,
+                ),
+                JetTab(
+                  styles: theme.tabThemeClassic.styles.copyWith(JetTabStyles(
+                    textStyle: theme.tabThemeClassic.styles.textStyle.copyWith(fontSize: 15),
+                      textSelectedStyle: theme.tabThemeClassic.styles.textSelectedStyle.copyWith(fontSize: 15)
+                  )),
+                  items: [
+                    JetTabItem(text: 'All', selected: true),
+                    JetTabItem(text: 'Video'),
+                    JetTabItem(text: 'Stores'),
+                    JetTabItem(text: 'Settings'),
+                    JetTabItem(text: 'Tools'),
+                    JetTabItem(text: 'Images')
+                  ],
+                ),
                 JetCard(
                     color: Color(0xffF7F9FA),
                     child: JetText.body(loremText, applyParagraph: false)),
-
                 JetCard(
                     child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                       JetText.headerLarge('Header Large'),
-                      JetText.body( loremText),
-                      JetText.body( loremText),
-                      JetText.headerMedium('Header Medium', applyParagraph: true),
-                      JetText.body( loremText),
-                      JetText.body( loremText),
+                      JetText.body(loremText),
+                      JetText.body(loremText),
+                      JetText.headerMedium('Header Medium',
+                          applyParagraph: true),
+                      JetText.body(loremText),
+                      JetText.body(loremText),
                       JetText.headerSmall('Header Small', applyParagraph: true),
-                      JetText.body( loremText),
-                        ])),
+                      JetText.body(loremText),
+                    ])),
                 Container(
                   height: 450,
                   child: JetCard(
@@ -132,38 +139,35 @@ class TextStylesPage extends StatelessWidget {
                             textStyle: TextStyle(
                                 fontSize: 16,
                                 //fontFamily: 'Arial',
-                                letterSpacing: 1.5
-                            ),
+                                letterSpacing: 1.5),
                           ),
                         ),
-                      )
-                  ),
+                      )),
                 ),
                 JetCard(
                     child: SelectableText.rich(
-                      TextSpan(
-                        text: '', // default text style
-                        children: <InlineSpan>[
-                          ...JetTextSpan.headerLarge(context, 'Header Large'),
-                          ...JetTextSpan.body(context, loremText),
-                          ...JetTextSpan.body(context, loremText),
-
-                          ...JetTextSpan.headerMedium(context, 'Header Medium', applyParagraph: true),
-                          ...JetTextSpan.body(context, loremText),
-                          ...JetTextSpan.body(context, loremText),
-
-                          ...JetTextSpan.headerSmall(context, 'Header Small', applyParagraph: true),
-                          ...JetTextSpan.body(context, loremText)
-                        ],
-                      ),
-
-                    )
-                )
+                  TextSpan(
+                    text: '', // default text style
+                    children: <InlineSpan>[
+                      ...JetTextSpan.headerLarge(context, 'Header Large'),
+                      ...JetTextSpan.body(context, loremText),
+                      ...JetTextSpan.body(context, loremText),
+                      ...JetTextSpan.headerMedium(context, 'Header Medium',
+                          applyParagraph: true),
+                      ...JetTextSpan.body(context, loremText),
+                      ...JetTextSpan.body(context, loremText),
+                      ...JetTextSpan.headerSmall(context, 'Header Small',
+                          applyParagraph: true),
+                      ...JetTextSpan.body(context, loremText)
+                    ],
+                  ),
+                ))
               ]),
             )),
       ),
     );
   }
 
-  final String loremText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  final String loremText =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 }
